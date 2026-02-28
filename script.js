87e6fef451f8d9d1a1d73fd9b422b9c9cab15567
Montaha Hossain <mdmontaha2019@gmail.com>
2026-02-28 12:50:35 +0600

    Button toggle added

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ let allJobs = document.getElementById("allJobs");
 
 totalCount.innerText = allJobs.children.length;
 jobsCorner.innerText = allJobs.children.length;
+
+// Buttons
+const tabActive = ["btn", "btn-primary"];
+// const tabActive = ["btn bg-white px-10 py-5"];
+const tabInactive = ["btn", "btn-soft"];
+
+let currentTab = "all";
+
+function switchTab(tab) {
+  console.log(tab);
+  const tabs = ["all", "interview", "rejected"];
+  for (const t of tabs) {
+    const tabName = document.getElementById("tab-" + t);
+    if (t === tab) {
+      tabName.classList.remove(...tabInactive);
+      tabName.classList.add(...tabActive);
+    } else {
+      tabName.classList.remove(...tabActive);
+      tabName.classList.add(...tabInactive);
+    }
+  }
+}
