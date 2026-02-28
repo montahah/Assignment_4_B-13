b0cb5a373618b354d656833fa11378b5db2daf71
Montaha Hossain <mdmontaha2019@gmail.com>
2026-02-28 22:21:40 +0600

    all section added with counts

--- script.js
+++ script.js
@@ -12,11 +12,13 @@ const tabInactive = ["btn", "btn-soft"];
 const allSection = document.getElementById("all-section");
 const interviewSection = document.getElementById("interview-section");
 const rejectSection = document.getElementById("reject-section");
+const emptyState = document.getElementById("empty-state");
 
 let currentTab = "all";
 
 function switchTab(tab) {
   const tabs = ["all", "interview", "rejected"];
+  currentTab = tab;
   for (const t of tabs) {
     const tabName = document.getElementById("tab-" + t);
     if (t === tab) {
@@ -29,19 +31,37 @@ function switchTab(tab) {
   }
   const pages = [allSection, interviewSection, rejectSection];
   for (const section of pages) {
-    section.classList.add("hidden");
+    section.classList.remove("hidden");
   }
+  emptyState.classList.add("hidden");
+
   if (tab === "all") {
     allSection.classList.remove("hidden");
+    interviewSection.classList.add("hidden");
+    rejectSection.classList.add("hidden");
+    if (allSection.children.length < 1) {
+      emptyState.classList.remove("hidden");
+    }
   } else if (tab === "interview") {
     interviewSection.classList.remove("hidden");
-  } else if (tab === "rejected") {
+    allSection.classList.add("hidden");
+    rejectSection.classList.add("hidden");
+    if (interviewSection.children.length < 1) {
+      emptyState.classList.remove("hidden");
+    }
+  } else {
     rejectSection.classList.remove("hidden");
+    interviewSection.classList.add("hidden");
+    allSection.classList.add("hidden");
+    if (rejectSection.children.length < 1) {
+      emptyState.classList.remove("hidden");
+    }
   }
+  updateStat();
 }
 
-totalCount.innerText = allSection.children.length;
-jobsCorner.innerText = allSection.children.length;
+// totalCount.innerText = allSection.children.length;
+// jobsCorner.innerText = allSection.children.length;
 
 switchTab(currentTab);
 
@@ -50,12 +70,40 @@ document
   .getElementById("all-sections")
   .addEventListener("click", function (event) {
     const clickedElement = event.target;
+    const card = clickedElement.closest(".Card");
+    const status = card.querySelector(".Status");
+    const parent = card.parentNode;
+    console.log(parent);
 
     if (clickedElement.classList.contains("interview-btn")) {
-      console.log("hello");
+      status.innerText = "Interviewed";
+      interviewSection.appendChild(card);
+      updateStat();
     } else if (clickedElement.classList.contains("rejected-btn")) {
-      console.log("hello");
+      status.innerText = "Rejected";
+      rejectSection.appendChild(card);
+      updateStat();
     } else if (clickedElement.classList.contains("delete-btn")) {
-      console.log("hello");
+      parent.removeChild(card);
+      updateStat();
     }
   });
+
+function updateStat() {
+  // totalCount.innerText = allSection.children.length;
+  // interviewCount.innerText = interviewSection.children.length;
+  // rejectCount.innerText = rejectSection.children.length;
+
+  const counts = {
+    all: allSection.children.length,
+    interview: interviewSection.children.length,
+    rejected: rejectSection.children.length,
+  };
+
+  totalCount.innerText = counts["all"];
+  interviewCount.innerText = counts["interview"];
+  rejectCount.innerText = counts["rejected"];
+
+  jobsCorner.innerText = counts[currentTab];
+}
+updateStat();
